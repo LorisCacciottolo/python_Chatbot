b7172f1f63af9a3ea629b531bd4838f4b71f05b4
Silver.btc <48631351+SilverBtc@users.noreply.github.com>
2023-12-22 00:55:53 +0100

    add some comments

--- src/TFIDFProcessor.py
+++ src/TFIDFProcessor.py
@@ -4,7 +4,12 @@ import re
 from src.calculate_tf import calculate_tf
 from src.calculate_idf import calculate_idf
 
+
 class TFIDFProcessor:
+    # @staticmethod
+    # The @staticmethod decorator indicates that this method is a static method of the class.
+    # This means the method can be called on the class itself, without needing an instance of the class.
+
 
     def __init__(self, directory):
         self.directory = directory
@@ -29,17 +34,22 @@ class TFIDFProcessor:
         print(self.directory)
         word_document_count = {}
 
+        # Iterates through each file in the directory.
         for file in os.listdir(self.directory):
             with open(f"{self.directory}/{file}", 'r', encoding='utf-8') as file:
+                # Creates sets of unique words from both the file and the question.
                 content = set(file.read().split())
                 content_question = set(question.split())
+                # Counts the number of documents in which each word in the question appears (Tf).
                 for word in content:
                     if word in content_question:
                         word_document_count[word] = word_document_count.get(word, 0) + 1
 
+        # Calculates IDF for each word in the question.
         for word, count in word_document_count.items():
             idf_dict[word] = math.log10(total_documents / count)
 
+        # Ensures all words in the question have an IDF value, assigning 0 if not present in the IDF dictionary.
         for i in question.split():
             if i not in idf_dict:
                 idf_dict[i] = 0
@@ -76,16 +86,21 @@ class TFIDFProcessor:
         return (scalar_A_B) / (NormA * NormB)
 
     def calculate_tfidf_matrix(self):
+        # Calculates IDF values
         idf_dict = calculate_idf(self.directory)
         tfidf_matrix = {}
 
+        # Iterates over files in the directory.
         for file in os.listdir(self.directory):
             file_path = f"{self.directory}/{file}"
+            # Reads each file and calculates its TF values.
             with open(file_path, 'r', encoding='utf-8') as file:
                 tf_dict = calculate_tf(file.read())
                 tfidf_vector = {}
+                # Calculates TF-IDF value for each word in the file.
                 for word, tf in tf_dict.items():
                     tfidf_vector[word] = tf * idf_dict.get(word, 0)
+                # Adds the file's TF-IDF vector to the matrix.
                 tfidf_matrix[file_path] = tfidf_vector
 
         return tfidf_matrix
@@ -94,8 +109,11 @@ class TFIDFProcessor:
         max_similarity = -1
         most_relevant_document_name = None
 
+        # Iterates over each document's TF-IDF vector in the corpus.
         for doc_name, tfidf_vector in self.tfidf_corpus.items():
+            # Calculates cosine similarity between the question's TF-IDF and the document's TF-IDF.
             similarity = TFIDFProcessor.cosine_similarity(tfidf_question, tfidf_vector)
+            # Updates the most relevant document if a higher similarity score is found.
             if similarity > max_similarity:
                 max_similarity = similarity
                 most_relevant_document_name = doc_name
@@ -107,11 +125,13 @@ class TFIDFProcessor:
 
     @staticmethod
     def find_highest_tfidf_word(tfidf_vector, document_path):
+        # Sorts the TF-IDF vector in descending order by TF-IDF value.
         sorted_tfidf = sorted(tfidf_vector.items(), key=lambda item: item[1], reverse=True)
 
         with open(document_path.replace("speeches","cleaned"), 'r', encoding='utf-8') as file:
             document_content = file.read().split()
 
+        # Iterates over the sorted TF-IDF vector to find the highest scoring word present in the document.
         for word, _ in sorted_tfidf:
             if word in document_content:
                 return word
@@ -120,8 +140,10 @@ class TFIDFProcessor:
 
     @staticmethod
     def extract_sentence_with_word(document_path, word):
+        # Opens the document
         with open(document_path, 'r', encoding='utf-8') as file:
             text = file.read()
+            # Splits the text into sentences
             sentences = text.split(".")
             for sentence in sentences:
                 if word in sentence.lower():
